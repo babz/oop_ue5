2cd657581f9d1a34337dd1d8f5615a6bb67022f0
babz <babz@babz-laptop.(none)>
2010-11-18 19:55:39 +0100

    class

---
+++ src/MyInt.java
@@ -0,0 +1,17 @@
+public class MyInt {
+	
+	private int i;
+	
+	public MyInt(int i){
+		this.i = i;
+	}
+	
+	//vergleicht die Zahlen
+	public int compareTo(int j){
+		
+	}
+	
+	public String toString(){
+		return i + "";
+	}
+}

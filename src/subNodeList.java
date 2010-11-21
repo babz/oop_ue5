3eca6d052535c21f44550a0b2e96678139623245
noce <e0827181@student.tuwien.ac.at>
2010-11-21 19:00:24 +0100

    stand der dinge

--- src/subNodeList.java
+++ src/subNodeList.java
@@ -1,35 +1,35 @@
 class subNodeList<A> implements TreeCollection<A> {
-	protected class Node { // list nodes
-		A elem; // element in this node
-		Node next = null; // next node in the list
+	protected class Node { 
+		A elem; 
+		Node next = null;
 
 		Node (A elem){ 
 			this.elem = elem; 
 		}
 	}
 	
-	protected Node head = null; // first node of list
-	protected Node tail = null; // last list node
+	protected Node head = null; 
+	protected Node tail = null; 
 
 	protected class ListIter implements Iter<A> {
-		protected Node p = head; // iterator position
-		public A next() { // get next list element
+		protected Node p = head; 
+		public A next() { 
 			if (p == null) return null;
 			A elem = p.elem;
 			p = p.next;
 			return elem;
 		}
-		public boolean hasNext() { // more elements?
+		public boolean hasNext() { 
 			return p != null;
 		}
 	}
 	
-	public void addNode (A x) { // add element to list
+	public void addNode (A x) {
 		if (head == null) tail = head = new Node(x);
 		else tail = tail.next = new Node(x);
 	}
 	
-	public Iter<A> assoc() { // new list iter.
+	public Iter<A> assoc() { 
 		return new ListIter();
 	}
 }

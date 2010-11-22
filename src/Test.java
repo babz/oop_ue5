8feed86dc78ea994d7aec3b4bb2c1be6f4124d3b
noce <e0827181@student.tuwien.ac.at>
2010-11-22 22:45:11 +0100

    erste testfälle

--- src/Test.java
+++ src/Test.java
@@ -39,10 +39,20 @@ public class Test{
 		 * (also auch Kanten, die nicht von der Wurzel ausgehen), entfernen Sie Kanten, und so weiter. 
 		 * ï¿½berprï¿½fen Sie die Korrektheit durch Ausgabe der ï¿½ber Iteratoren ermittelten Labels.
 		 */
-		Tree tree;
+		Tree intTree;
+		MyInt myint1 = new MyInt(6);
+		MyInt myint2 = new MyInt(3);
+		MyInt myint3 = new MyInt(4);
+		MyInt myint4 = new MyInt(8);
+		MyInt myint5 = new MyInt(9);
 		
-		checkIter(tree.allLabels(), "bla", "bli", "blu");
+		intTree = new Tree(myInt1);
 		
+		
+//		checkIter(tree.allLabels(), "bla", "bli", "blu");
+		
+
+			
 		/* 2:
 		 * Erzeugen Sie einen Baum als Instanz von ValueTree, in dem Labels auf Kanten vom Typ ADescriptor 
 		 * und Werte der Knoten vom Typ BDescriptor sind, und machen Sie entsprechende ï¿½berprï¿½fungen wie in 
@@ -50,6 +60,27 @@ public class Test{
 		 * von Knoten aus. 
 		 */
 		
+		ADescriptor ades1 = new ADescriptor("Herwig isst Schokolade");
+		ADescriptor ades2 = new ADescriptor("Alle Kinder mögen Schokolade");
+		ADescriptor ades3 = new ADescriptor("Am besten ist aber Kinder Schokolade");
+		ADescriptor ades4 = new ADescriptor("und Milka");
+		ADescriptor ades5 = new ADescriptor("des Rest isst nix");
+		
+		BDescriptor bdes1 = new BDescriptor("Bald ist Weihnachten");
+		BDescriptor bdes2 = new BDescriptor("Da wirds kalt");
+		BDescriptor bdes3 = new BDescriptor("aber nur mit Schlagobers");
+		BDescriptor bdes4 = new BDescriptor("aber jetzt");
+		
+		int aint1 = 1;
+		int aint2 = 2;
+		int aint3 = 3;
+		int aint4 = 1;
+		int aint5 = 0;
+		
+		boolean bbool1 = true;
+		boolean bbool2 = false;
+		boolean bbool3 = true;
+		boolean bbool4 = true;
 		
 		/* 3:
 		 * Falls ValueTree mit entsprechenden Typparameterersetzungen ein Untertyp von Tree ist, 
@@ -62,6 +93,9 @@ public class Test{
 		 * diesen Typen keine Untertypbeziehung bestehen kann.
 		 */
 		
+		
+		
+		
 		/* 4:
 		 * Erzeugen Sie einen weiteren Baum als Instanz von Tree, in dem Labels auf Kanten vom Typ 
 		 * Descriptor sind. Lesen Sie alle Labels auf Kanten und Werte von Knoten vom in Punkt 2 erzeugten 
@@ -69,6 +103,20 @@ public class Test{
 		 * ADescriptor als auch von BDescriptor wenn ungleich null) als Labels von Kanten in den neuen Baum 
 		 * ein. Lesen Sie die Labels der Kanten ï¿½ber Iteratoren aus.
 		 */
-
+		
+		ADescriptor ades6 = new ADescriptor("solalala");
+		ADescriptor ades7 = new ADescriptor("jaja");
+		ADescriptor ades8 = new ADescriptor("kein b");
+		
+		BDescriptor bdes5 = new BDescriptor("mit b");
+		BDescriptor bdes6 = new BDescriptor("geht nicht");
+		
+		int aint6 = 3;
+		int aint7 = 2;
+		int aint8 = 0;
+		
+		boolean bbool5 = true;
+		boolean bbool6 = false;
+		
 	}
 }
